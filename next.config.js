c09a3605fd995bf498879d188d07e1806ae737b0
Ashik Shafayet <ashik19aug@gmail.com>
2021-03-11 16:52:15 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://Ash:as74107410@cluster0.jenol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        // MONGO_URI: "mongodb://192.168.10.63:27017/rnd_mysql_mongo?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+        MONGO_URI: "mongo url"
     }
 }
